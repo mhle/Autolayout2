3d6d1fb8c8d410a29ca0111d9f4007f3f8dee15a
Michael Le <mle@michaels-mbp.home>
2017-02-10 00:52:39 +0000

    initial commit

---
+++ Autolayout2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Autolayout2
+//
+//  Created by Michael Le on 10/02/2017.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
